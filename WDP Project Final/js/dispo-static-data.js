adb4a2664bdefffe9b1eda406b42368fc0568b07
DCorleone <DCorleone@gmx.de>
2015-02-09 20:56:01 +0100

    first project import (WebStorm)

--- WDP Project Final/js/dispo-static-data.js
+++ WDP Project Final/js/dispo-static-data.js
@@ -16,3 +16,22 @@ customers.push(new Customer("Curecomp", new Address("Hafenstrasse", "49-51", "40
 customers.push(new Customer("ZF-Passau", new Address("Donaustrasse", "25", "94034", "Passau", "DE", "Germany")));
 
 contacts.push(new Contact("Thomas", "Herzog", "herzog.thomas81@gmail.com", "+43664123456789"));
+
+var customerSelection = (function () {
+    var
+        _handler = this
+        ,
+        _$ = $;
+
+    this.init = function () {
+        for (var i = 0; i < customers.length; i++) {
+            $('<option/>', {
+                'value': customers[i].name,
+                'text': customers[i].name
+            }).appendTo('custSel');
+        }//for
+    }();
+
+
+}());
+
