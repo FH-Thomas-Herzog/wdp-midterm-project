fd2530718ec3c0cb5cbdc6602cba9d1037f0a929
DCorleone <DCorleone@gmx.de>
2015-02-10 22:02:18 +0100

    first project import (WebStorm)

--- WDP Project Final/js/dispo-static-data.js
+++ WDP Project Final/js/dispo-static-data.js
@@ -11,6 +11,11 @@ var
      * @type {Array}
      */
     contacts = [];
+/**
+ * Test Positions
+ * @type {Array}
+ */
+positions = [];
 
 /* create static data for customer selection */
 customers.push(new Customer("Curecomp", new Address("Hafenstrasse", "49-51", "4020", "Linz", "AT", "Austria")));
@@ -18,6 +23,9 @@ customers.push(new Customer("ZF-Passau", new Address("Donaustrasse", "25", "9403
 
 contacts.push(new Contact("Thomas", "Herzog", "herzog.thomas81@gmail.com", "+43664123456789"));
 
+positions.push(new DispoPosition("0000", "Item No 1", "3", "2"));
+positions.push(new DispoPosition("0001", "Item No 2", "1", "8"));
+positions.push(new DispoPosition("0002", "Item No 3", "5", "9"));
 
 $(function () {
     var renderer = rendererHelperSingletonFactory.get();
